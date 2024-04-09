b489f66f488bb0cc6aae60b6caa04556dc7f0066
nora-shirokuma <myself@nora-shirokuma.com>
2024-04-09 12:49:59 +0900

    PHPDoc追記。

--- src/Domain/AbstractBool.php
+++ src/Domain/AbstractBool.php
@@ -7,6 +7,10 @@ namespace NoraShirokuma\CommonPhp\Domain;
  */
 abstract class AbstractBool
 {
+    /**
+     * 内包するBool値
+     * @var bool|null
+     */
     protected ?bool $value;
 
     /**
@@ -17,11 +21,19 @@ abstract class AbstractBool
         $this->value = $value;
     }
 
+    /**
+     * 内包するBool値を取得します
+     * @return bool|null
+     */
     public function value(): ?bool
     {
         return $this->value;
     }
 
+    /**
+     * @param int $value
+     * @return bool
+     */
     public static function int2Bool(int $value): bool
     {
         return $value === 1;
